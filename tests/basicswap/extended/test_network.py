b5a4df990812acdf5451230b5b257ea2c4b076e8
tecnovert <tecnovert@tecnovert.net>
2023-03-13 21:27:36 +0200

    tests: Fix network test.

--- tests/basicswap/extended/test_network.py
+++ tests/basicswap/extended/test_network.py
@@ -221,8 +221,8 @@ class Test(unittest.TestCase):
                 else:
                     rpc('extkeyimportmaster', [rpc('mnemonic', ['new'])['master']])
                 # Lower output split threshold for more stakeable outputs
-                rpc('walletsettings', ['stakingoptions', json.dumps({'stakecombinethreshold': 100, 'stakesplitthreshold': 200}).replace('"', '\\"')])
-                rpc('reservebalance', ['false'])
+                rpc('walletsettings', ['stakingoptions', {'stakecombinethreshold': 100, 'stakesplitthreshold': 200}])
+                rpc('reservebalance', [False,])
 
             for i in range(NUM_BTC_NODES):
                 data_dir = prepareDataDir(TEST_DIR, i, 'bitcoin.conf', 'btc_', base_p2p_port=BTC_BASE_PORT, base_rpc_port=BTC_BASE_RPC_PORT)
