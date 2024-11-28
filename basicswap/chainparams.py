1797db97a0a3c3e8b8e6549a0a36951ea5f12a06
nahuhh <50635951+nahuhh@users.noreply.github.com>
2024-11-28 12:59:46 +0000

    doge: chainparams

--- basicswap/chainparams.py
+++ basicswap/chainparams.py
@@ -31,6 +31,7 @@ class Coins(IntEnum):
     LTC_MWEB = 15
     # ZANO = 16
     BCH = 17
+    DOGE = 18
 
 
 chainparams = {
@@ -149,6 +150,44 @@ chainparams = {
             "key_prefix": 239,
             "hrp": "rltc",
             "bip44": 1,
+            "min_amount": 100000,
+            "max_amount": 10000000 * COIN,
+        },
+    },
+    Coins.DOGE: {
+        "name": "dogecoin",
+        "ticker": "DOGE",
+        "message_magic": "Dogecoin Signed Message:\n",
+        "blocks_target": 60 * 1,
+        "decimal_places": 8,
+        "mainnet": {
+            "rpcport": 22555,
+            "pubkey_address": 30,
+            "script_address": 22,
+            "key_prefix": 158,
+            "hrp": "doge",
+            "bip44": 3,
+            "min_amount": 100000,  # TODO increase above fee
+            "max_amount": 10000000 * COIN,
+        },
+        "testnet": {
+            "rpcport": 44555,
+            "pubkey_address": 113,
+            "script_address": 196,
+            "key_prefix": 239,
+            "hrp": "tdge",
+            "bip44": 3,
+            "min_amount": 100000,
+            "max_amount": 10000000 * COIN,
+            "name": "testnet4",
+        },
+        "regtest": {
+            "rpcport": 18332,
+            "pubkey_address": 113,
+            "script_address": 196,
+            "key_prefix": 239,
+            "hrp": "rdge",
+            "bip44": 3,
             "min_amount": 100000,
             "max_amount": 10000000 * COIN,
         },
