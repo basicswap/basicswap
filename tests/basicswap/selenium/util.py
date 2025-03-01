5ce607541e95dedd3886d7b21752363f52ba9848
tecnovert <tecnovert@tecnovert.net>
2025-03-01 22:38:54 +0200

    tests: Run selenium test in CI

--- tests/basicswap/selenium/util.py
+++ tests/basicswap/selenium/util.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2023 tecnovert
-# Copyright (c) 2024 The Basicswap developers
+# Copyright (c) 2024-2025 The Basicswap developers
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
@@ -20,19 +20,24 @@ BSX_SELENIUM_DRIVER = os.getenv("BSX_SELENIUM_DRIVER", "firefox")
 def get_driver():
     if BSX_SELENIUM_DRIVER == "firefox":
         from selenium.webdriver import Firefox, FirefoxOptions
-
-        driver = Firefox(options=FirefoxOptions())
+        options = FirefoxOptions()
+        driver = Firefox(options=options)
+    elif BSX_SELENIUM_DRIVER == "firefox-ci":
+        from selenium.webdriver import Firefox, FirefoxOptions
+        options = FirefoxOptions()
+        options.headless = True
+        options.add_argument("start-maximized")
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        driver = Firefox(options=options)
     elif BSX_SELENIUM_DRIVER == "chrome":
         from selenium.webdriver import Chrome, ChromeOptions
-
         driver = Chrome(options=ChromeOptions())
     elif BSX_SELENIUM_DRIVER == "safari":
         from selenium.webdriver import Safari, SafariOptions
-
         driver = Safari(options=SafariOptions())
     else:
         raise ValueError("Unknown driver " + BSX_SELENIUM_DRIVER)
-
     return driver
 
 
