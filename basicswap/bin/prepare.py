26392eafb48bebf9e74fb051a527448b6d5f7c8c
nahuhh <50635951+nahuhh@users.noreply.github.com>
2025-10-02 18:32:33 +0000

    firo: v0.14.14.3 emergency release

--- basicswap/bin/prepare.py
+++ basicswap/bin/prepare.py
@@ -85,7 +85,7 @@ PIVX_VERSION_TAG = os.getenv("PIVX_VERSION_TAG", "")
 DASH_VERSION = os.getenv("DASH_VERSION", "22.1.3")
 DASH_VERSION_TAG = os.getenv("DASH_VERSION_TAG", "")
 
-FIRO_VERSION = os.getenv("FIRO_VERSION", "0.14.14.1")
+FIRO_VERSION = os.getenv("FIRO_VERSION", "0.14.14.3")
 FIRO_VERSION_TAG = os.getenv("FIRO_VERSION_TAG", "")
 
 NAV_VERSION = os.getenv("NAV_VERSION", "7.0.3")
