8699f1350a8aee89cabd550404eb6b4f6218aa65
tecnovert <tecnovert@tecnovert.net>
2026-09-07 08:39:09 +0200

    refactor: thread db cursor through fundTx and fundSCLockTx

--- basicswap/basicswap.py
+++ basicswap/basicswap.py
@@ -7105,7 +7105,11 @@ class BasicSwap(BaseApp, BSXNetwork, UIApp):
                     bid.amount, xmr_swap.a_lock_tx_script, xmr_swap.vkbv
                 )
                 xmr_swap.a_lock_tx = ci_from.fundSCLockTx(
-                    xmr_swap.a_lock_tx, a_fee_rate, xmr_swap.vkbv, bid_id=bid.bid_id
+                    xmr_swap.a_lock_tx,
+                    a_fee_rate,
+                    xmr_swap.vkbv,
+                    bid_id=bid.bid_id,
+                    cursor=use_cursor,
                 )
                 funded_a_lock_tx = xmr_swap.a_lock_tx
 
--- basicswap/interface/dcr/dcr.py
+++ basicswap/interface/dcr/dcr.py
@@ -1130,6 +1130,7 @@ class DCRInterface(FeeValidator, Secp256k1Interface):
         lock_unspents: bool = True,
         subfee: bool = False,
         bid_id: bytes = None,
+        cursor=None,
     ) -> bytes:
         if subfee:
             # dcrwallet's fundrawtransaction has no subtractFeeFromOutputs option,
@@ -1213,8 +1214,10 @@ class DCRInterface(FeeValidator, Secp256k1Interface):
         tx.vout.append(self.txoType()(value, self.getScriptDest(script)))
         return tx.serialize()
 
-    def fundSCLockTx(self, tx_bytes, feerate, vkbv=None, bid_id: bytes = None):
-        return self.fundTx(tx_bytes, feerate)
+    def fundSCLockTx(
+        self, tx_bytes, feerate, vkbv=None, bid_id: bytes = None, cursor=None
+    ):
+        return self.fundTx(tx_bytes, feerate, bid_id=bid_id, cursor=cursor)
 
     def genScriptLockRefundTxScript(self, Kal, Kaf, csv_val) -> bytes:
         ensure(len(Kal) == 33, "invalid Kal length")
